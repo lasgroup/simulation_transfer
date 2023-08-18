5c6c75c1db7ec89fe22ff53915888441d0451aec
Jonas Rothfuss <jonas.rothfuss@gmail.com>
2023-08-18 15:27:11 +0200

    add recorded/real rccar data to data_provider

--- experiments/data_provider.py
+++ experiments/data_provider.py
@@ -1,6 +1,14 @@
 from typing import Dict, Any
-import jax
+from functools import partial
 import jax.numpy as jnp
+import jax
+import os
+
+from sim_transfer.sims.util import encode_angles as encode_angles_fn
+from experiments.util import load_csv_recordings
+
+DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data')
+
 
 DEFAULTS_SINUSOIDS = {
     'obs_noise_std': 0.1,
@@ -20,6 +28,12 @@ DEFAULTS_RACECAR = {
     'x_support_mode_train': 'full',
     'param_mode': 'random'
 }
+
+DEFAULTS_RACECAR_REAL = {
+    'sampling': 'consecutive',
+    'num_samples_test': 4000
+}
+
 _RACECAR_NOISE_STD_ENCODED = jnp.concatenate([DEFAULTS_RACECAR['obs_noise_std'][:2],
                                               DEFAULTS_RACECAR['obs_noise_std'][2:3],
                                               DEFAULTS_RACECAR['obs_noise_std'][2:3],
@@ -57,17 +71,48 @@ DATASET_CONFIGS = {
 }
 
 
+def get_rccar_recorded_data(encode_angle: bool = True, skip_first_n_points: int = 30):
+    recordings_dir = os.path.join(DATA_DIR, 'recordings_rc_car_v0')
+    recording_dfs = load_csv_recordings(recordings_dir)
+
+    def prepare_rccar_data(df, encode_angles: bool = False, change_signs: bool = True,
+                           skip_first_n: int = 30):
+        u = df[['steer', 'throttle']].to_numpy()
+        x = df[['pos x', 'pos y', 'theta', 's vel x', 's vel y', 's omega']].to_numpy()
+        # project theta into [-\pi, \pi]
+        if change_signs:
+            x[:, [1, 4]] *= -1
+        x[:, 2] = (x[:, 2] + jnp.pi) % (2 * jnp.pi) - jnp.pi
+        if encode_angles:
+            x = encode_angles_fn(x, angle_idx=2)
+        # remove first n steps (since often not much is happening)
+        x, u = x[skip_first_n:], u[skip_first_n:]
+
+        x_data = jnp.concatenate([x[:-1], u[:-1]], axis=-1)  # current state + action
+        y_data = x[1:]  # next state
+        assert x_data.shape[0] == y_data.shape[0]
+        assert x_data.shape[1] - 2 == y_data.shape[1]
+        return x_data, y_data
+
+    num_train_traj = 1
+    prep_fn = partial(prepare_rccar_data, encode_angles=encode_angle, skip_first_n=skip_first_n_points)
+    x_train, y_train = map(lambda x: jnp.concatenate(x, axis=0), zip(*map(prep_fn, recording_dfs[:num_train_traj])))
+    x_test, y_test = map(lambda x: jnp.concatenate(x, axis=0), zip(*map(prep_fn, recording_dfs[num_train_traj:])))
+
+    return x_train, y_train, x_test, y_test
+
+
 def provide_data_and_sim(data_source: str, data_spec: Dict[str, Any], data_seed: int = 845672):
     # load data
     key_train, key_test = jax.random.split(jax.random.PRNGKey(data_seed), 2)
     if data_source == 'sinusoids1d' or data_source == 'sinusoids2d':
         from sim_transfer.sims.simulators import SinusoidsSim
-        DEFAULTS = DEFAULTS_SINUSOIDS
+        defaults = DEFAULTS_SINUSOIDS
         sim_hf = sim_lf = SinusoidsSim(input_size=1, output_size=int(data_source[-2]))
         assert {'num_samples_train'} <= set(data_spec.keys()) <= {'num_samples_train'}.union(DEFAULTS_SINUSOIDS.keys())
     elif data_source == 'pendulum' or data_source == 'pendulum_hf':
         from sim_transfer.sims.simulators import PendulumSim
-        DEFAULTS = DEFAULTS_PENDULUM
+        defaults = DEFAULTS_PENDULUM
         if data_source == 'pendulum_hf':
             sim_hf = PendulumSim(encode_angle=True, high_fidelity=True)
             sim_lf = PendulumSim(encode_angle=True, high_fidelity=False)
@@ -76,7 +121,7 @@ def provide_data_and_sim(data_source: str, data_spec: Dict[str, Any], data_seed:
         assert {'num_samples_train'} <= set(data_spec.keys()) <= {'num_samples_train'}.union(DEFAULTS_PENDULUM.keys())
     elif data_source == 'pendulum_bimodal' or data_source == 'pendulum_bimodal_hf':
         from sim_transfer.sims.simulators import PendulumBiModalSim
-        DEFAULTS = DEFAULTS_PENDULUM
+        defaults = DEFAULTS_PENDULUM
         if data_source == 'pendulum_bimodal_hf':
             sim_hf = PendulumBiModalSim(encode_angle=True, high_fidelity=True)
             sim_lf = PendulumBiModalSim(encode_angle=True, high_fidelity=False)
@@ -85,13 +130,42 @@ def provide_data_and_sim(data_source: str, data_spec: Dict[str, Any], data_seed:
         assert {'num_samples_train'} <= set(data_spec.keys()) <= {'num_samples_train'}.union(DEFAULTS_PENDULUM.keys())
     elif data_source == 'racecar' or data_source == 'racecar_hf':
         from sim_transfer.sims.simulators import RaceCarSim
-        DEFAULTS = DEFAULTS_RACECAR
+        defaults = DEFAULTS_RACECAR
         if data_source == 'racecar_hf':
             sim_hf = RaceCarSim(encode_angle=True, use_blend=True)
             sim_lf = RaceCarSim(encode_angle=True, use_blend=False)
         else:
             sim_hf = sim_lf = RaceCarSim(encode_angle=True, use_blend=True)
         assert {'num_samples_train'} <= set(data_spec.keys()) <= {'num_samples_train'}.union(DEFAULTS_RACECAR.keys())
+    elif data_source == 'rccar_real':
+        from sim_transfer.sims.simulators import RaceCarSim
+        sim_lf = RaceCarSim(encode_angle=True, use_blend=True)
+        x_train, y_train, x_test, y_test = get_rccar_recorded_data(encode_angle=True)
+        num_train_available = x_train.shape[0]
+        num_test_available = x_test.shape[0]
+
+        num_train = data_spec['num_samples_train']
+        num_test = data_spec.get('num_samples_test', DEFAULTS_RACECAR_REAL['num_samples_test'])
+        assert num_train <= num_train_available and num_test <= num_test_available
+        sampling_scheme = data_spec.get('sampling', DEFAULTS_RACECAR_REAL['sampling'])
+        if sampling_scheme == 'iid':
+            # sample random subset (datapoints are not adjacent in time)
+            assert num_train <= num_train_available / 4., f'Not enough data for {num_train} iid samples.' \
+                                                f'Requires at lest 4 times as much data as requested iid samples.'
+            idx_train = jax.random.choice(key_train, jnp.arange(num_train_available), shape=(num_train,), replace=False)
+            idx_test = jax.random.choice(key_test, jnp.arange(num_test_available), shape=(num_test,), replace=False)
+        elif sampling_scheme == 'consecutive':
+            # sample random sub-trajectory (datapoints are adjacent in time -> highly correlated)
+            offset_train = jax.random.choice(key_train, jnp.arange(num_train_available - num_train))
+            offset_test = jax.random.choice(key_test, jnp.arange(num_test_available - num_test))
+            idx_train = jnp.arange(num_train) + offset_train
+            idx_test = jnp.arange(num_test) + offset_test
+        else:
+            raise ValueError(f'Unknown sampling scheme {sampling_scheme}. Needs to be one of ["iid", "consecutive"].')
+
+        x_train, y_train, x_test, y_test = x_train[idx_train], y_train[idx_train], x_test[idx_test], y_test[idx_test]
+        return x_train, y_train, x_test, y_test, sim_lf
+
     else:
         raise ValueError('Unknown data source %s' % data_source)
 
@@ -99,8 +173,8 @@ def provide_data_and_sim(data_source: str, data_spec: Dict[str, Any], data_seed:
         rng_key=key_train,
         num_samples_train=data_spec['num_samples_train'],
         num_samples_test=1000,
-        obs_noise_std=data_spec.get('obs_noise_std', DEFAULTS['obs_noise_std']),
-        x_support_mode_train=data_spec.get('x_support_mode_train', DEFAULTS['x_support_mode_train']),
-        param_mode=data_spec.get('param_mode', DEFAULTS['param_mode'])
+        obs_noise_std=data_spec.get('obs_noise_std', defaults['obs_noise_std']),
+        x_support_mode_train=data_spec.get('x_support_mode_train', defaults['x_support_mode_train']),
+        param_mode=data_spec.get('param_mode', defaults['param_mode'])
     )
     return x_train, y_train, x_test, y_test, sim_lf
