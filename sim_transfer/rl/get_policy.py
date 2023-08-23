02f162579c9aa75ef6b22d591ff904dc8960616e
lenarttreven <lenart.treven44@gmail.com>
2023-08-23 09:47:34 +0200

    prepare params for bhavy

--- sim_transfer/rl/get_policy.py
+++ sim_transfer/rl/get_policy.py
@@ -1,18 +1,15 @@
-from datetime import datetime
+import pickle
 
 import jax.numpy as jnp
 import jax.random as jr
 import jax.tree_util as jtu
-import matplotlib.pyplot as plt
 from brax.training.replay_buffers import UniformSamplingQueue
 from brax.training.types import Transition
-from jax import jit, vmap
-from jax.lax import scan
+from jax import vmap
 from mbpo.optimizers.policy_optimizers.sac.sac import SAC
 from mbpo.systems.brax_wrapper import BraxWrapper
 
 from sim_transfer.sims.car_system import CarSystem
-from sim_transfer.sims.util import plot_rc_trajectory
 
 ENCODE_ANGLE = True
 system = CarSystem(encode_angle=ENCODE_ANGLE,
@@ -49,8 +46,6 @@ env = BraxWrapper(system=system,
                   sample_buffer=sampling_buffer,
                   system_params=system.init_params(jr.PRNGKey(0)), )
 
-state = jit(env.reset)(rng=jr.PRNGKey(0))
-
 num_env_steps_between_updates = 32
 num_envs = 16
 horizon = 300
@@ -85,73 +80,11 @@ sac_trainer = SAC(
     wandb_logging=False,
 )
 
-max_y = 0
-min_y = -100
-
-xdata, ydata = [], []
-times = [datetime.now()]
-
-
-def progress(num_steps, metrics):
-    times.append(datetime.now())
-    xdata.append(num_steps)
-    ydata.append(metrics['eval/episode_reward'])
-    # plt.xlim([0, sac_trainer.num_timesteps])
-    # plt.ylim([min_y, max_y])
-    plt.xlabel('# environment steps')
-    plt.ylabel('reward per episode')
-    plt.plot(xdata, ydata)
-    plt.show()
-
-
-params, metrics = sac_trainer.run_training(key=jr.PRNGKey(0), progress_fn=progress)
-
 make_inference_fn = sac_trainer.make_policy
 
-print(f'time to jit: {times[1] - times[0]}')
-print(f'time to train: {times[-1] - times[1]}')
+with open('params.pkl', 'rb') as file:
+    params = pickle.load(file)
 
 
 def policy(x):
     return make_inference_fn(params, deterministic=True)(x, jr.PRNGKey(0))[0]
-
-
-test_system = CarSystem(encode_angle=ENCODE_ANGLE,
-                        action_delay=0.00,
-                        use_tire_model=True,
-                        use_obs_noise=True,
-                        ctrl_cost_weight=0.005,
-                        )
-
-system_state_init = system.reset(key=jr.PRNGKey(0))
-x_init = system_state_init.x_next
-system_params = system_state_init.system_params
-
-
-def step(system_state, _):
-    u = policy(system_state.x_next)
-    next_sys_state = test_system.step(system_state.x_next, u, system_state.system_params)
-    return next_sys_state, (system_state.x_next, u, next_sys_state.reward)
-
-
-x_last, trajectory = scan(step, system_state_init, None, length=horizon)
-
-plt.plot(trajectory[0], label='Xs')
-plt.plot(trajectory[1], label='Us')
-plt.plot(trajectory[2], label='Rewards')
-plt.legend()
-plt.show()
-print('Reward: ', jnp.sum(trajectory[2]))
-
-traj = trajectory[0]
-actions = trajectory[1]
-
-plot_rc_trajectory(traj, actions, encode_angle=ENCODE_ANGLE)
-
-SAVE = True
-
-if SAVE:
-    import pickle
-
-    with open('params.pkl', 'wb') as file:
-        pickle.dump(params, file)
