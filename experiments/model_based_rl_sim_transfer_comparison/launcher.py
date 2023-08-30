a894885e1795f6cda11b6f807d0fc7338c6bb6f3
lenarttreven <lenart.treven44@gmail.com>
2023-08-30 09:48:51 +0200

    rename project_name in launcher.py

--- experiments/model_based_rl_sim_transfer_comparison/launcher.py
+++ experiments/model_based_rl_sim_transfer_comparison/launcher.py
@@ -1,7 +1,7 @@
 import exp
 from experiments.util import generate_run_commands, generate_base_command, dict_permutations
 
-PROJECT_NAME = 'ModelBasedRLSimTransferComparisonReproducingN5'
+PROJECT_NAME = 'ModelBasedRLSimTransferComparisonReproducingN6'
 
 applicable_configs = {
     'horizon_len': [8, 16, 32],
