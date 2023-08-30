1a29abcdbce5e43d807f4b336872dadb18ada33b
lenarttreven <lenart.treven44@gmail.com>
2023-08-30 14:48:34 +0200

    launcher.py ready for euler

--- experiments/model_based_rl_sim_transfer_comparison/launcher.py
+++ experiments/model_based_rl_sim_transfer_comparison/launcher.py
@@ -1,7 +1,7 @@
 import exp
 from experiments.util import generate_run_commands, generate_base_command, dict_permutations
 
-PROJECT_NAME = 'TestNOSACKwargsInMain'
+PROJECT_NAME = 'TestFinalVersion'
 
 applicable_configs = {
     'horizon_len': [8, 16, 32],
