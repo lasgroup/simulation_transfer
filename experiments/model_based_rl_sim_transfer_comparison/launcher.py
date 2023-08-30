324af517978db11d3efe499e8f1cee498405c6af
lenarttreven <lenart.treven44@gmail.com>
2023-08-30 16:25:42 +0200

    changes from merging branch

--- experiments/model_based_rl_sim_transfer_comparison/launcher.py
+++ experiments/model_based_rl_sim_transfer_comparison/launcher.py
@@ -1,7 +1,7 @@
 import exp
 from experiments.util import generate_run_commands, generate_base_command, dict_permutations
 
-PROJECT_NAME = 'TestFinalVersionNX'
+PROJECT_NAME = 'TestFinalVersionNXwithLogging'
 
 applicable_configs = {
     'horizon_len': [50, 2 ** 6, 2 ** 7],
