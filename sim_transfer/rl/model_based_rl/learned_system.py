6625c6fbc144499a2a73e8f70568a400f760ce3f
lenarttreven <lenart.treven44@gmail.com>
2023-08-31 17:53:47 +0200

    add frame stack to LearnedDynamics class

--- sim_transfer/rl/model_based_rl/learned_system.py
+++ sim_transfer/rl/model_based_rl/learned_system.py
@@ -22,31 +22,44 @@ class LearnedDynamics(Dynamics[DynamicsParams]):
                  u_dim: int,
                  model: BatchedNeuralNetworkModel,
                  include_noise: bool = True,
-                 predict_difference: bool = True
+                 predict_difference: bool = True,
+                 num_frame_stack: int = 0
                  ):
         Dynamics.__init__(self, x_dim=x_dim, u_dim=u_dim)
         self.model = model
         self.include_noise = include_noise
         self.predict_difference = predict_difference
+        self.num_frame_stack = num_frame_stack
+        self._x_dim = x_dim - u_dim * num_frame_stack
+        self._u_dim = u_dim
 
     def next_state(self,
                    x: chex.Array,
                    u: chex.Array,
                    dynamics_params: DynamicsParams) -> Tuple[Distribution, DynamicsParams]:
-        assert x.shape == (self.x_dim,) and u.shape == (self.u_dim,)
+        assert x.shape == (self._x_dim + self._u_dim * self.num_frame_stack,) and u.shape == (self._u_dim,)
         # Create state-action pair
         z = jnp.concatenate([x, u])
         z = z.reshape((1, -1))
+        next_key, key_sample_x_next = jr.split(dynamics_params.key)
         if self.predict_difference:
             delta_x_dist = self.model.predict_dist(z, include_noise=self.include_noise)
-            next_key, key_sample_x_next = jr.split(dynamics_params.key)
             delta_x = delta_x_dist.sample(seed=key_sample_x_next)
-            x_next = x + delta_x.reshape((self.x_dim,))
+            _x = x[:self._x_dim]
+            _x_next = _x + delta_x.reshape((self._x_dim,))
         else:
             x_next_dist = self.model.predict_dist(z, include_noise=self.include_noise)
-            next_key, key_sample_x_next = jr.split(dynamics_params.key)
-            x_next = x_next_dist.sample(seed=key_sample_x_next)
-            x_next = x_next.reshape((self.x_dim,))
+            _x_next = x_next_dist.sample(seed=key_sample_x_next)
+            _x_next = _x_next.reshape((self._x_dim,))
+
+        # Update last num_frame_stack actions
+        _us = x[self._x_dim:]
+        _us = jnp.roll(_us, shift=self._u_dim)
+        _us = _us.at[:self._u_dim].set(u)
+
+        # Concatenate state and last num_frame_stack actions
+        x_next = jnp.concatenate([_x_next, _us])
+
         new_dynamics_params = dynamics_params.replace(key=next_key)
         return Normal(loc=x_next, scale=jnp.zeros_like(x_next)), new_dynamics_params
 
@@ -59,11 +72,17 @@ class LearnedCarSystem(System[DynamicsParams, CarRewardParams]):
                  model: BatchedNeuralNetworkModel,
                  include_noise: bool,
                  predict_difference: bool,
+                 num_frame_stack: int = 0,
                  **car_reward_kwargs: dict):
+        self.num_frame_stack = num_frame_stack
         reward = CarReward(**car_reward_kwargs)
-        dynamics = LearnedDynamics(x_dim=reward.x_dim, u_dim=reward.u_dim, model=model, include_noise=include_noise,
-                                   predict_difference=predict_difference)
+        dynamics = LearnedDynamics(x_dim=reward.x_dim + self.num_frame_stack * reward.u_dim,
+                                   u_dim=reward.u_dim, model=model, include_noise=include_noise,
+                                   predict_difference=predict_difference,
+                                   num_frame_stack=num_frame_stack)
         System.__init__(self, dynamics=dynamics, reward=CarReward(**car_reward_kwargs))
+        self._x_dim = reward.x_dim
+        self._u_dim = reward.u_dim
 
     @staticmethod
     def system_params_vmap_axes(axes: int = 0):
@@ -76,10 +95,13 @@ class LearnedCarSystem(System[DynamicsParams, CarRewardParams]):
              u: chex.Array,
              system_params: SystemParams[DynamicsParams, CarRewardParams],
              ) -> SystemState:
+        assert x.shape == (self._x_dim + self._u_dim * self.num_frame_stack,) and u.shape == (self._u_dim,)
         new_key, key_x_next, key_reward = jr.split(system_params.key, 3)
         x_next_dist, next_dynamics_params = self.dynamics.next_state(x, u, system_params.dynamics_params)
         x_next = x_next_dist.sample(seed=key_x_next)
-        reward_dist, next_reward_params = self.reward(x, u, system_params.reward_params, x_next)
+        _x = x[:self._x_dim]
+        _x_next = x_next[:self._x_dim]
+        reward_dist, next_reward_params = self.reward(_x, u, system_params.reward_params, _x_next)
         return SystemState(x_next=x_next,
                            reward=reward_dist.sample(seed=key_reward),
                            system_params=SystemParams(dynamics_params=next_dynamics_params,
