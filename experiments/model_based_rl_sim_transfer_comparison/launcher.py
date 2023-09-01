2db4d2178f97e453cff619062ef514d416d2e51f
lenarttreven <lenart.treven44@gmail.com>
2023-09-01 12:09:08 +0200

    change project name in launcher.py

--- experiments/model_based_rl_sim_transfer_comparison/launcher.py
+++ experiments/model_based_rl_sim_transfer_comparison/launcher.py
@@ -1,7 +1,7 @@
 import exp
 from experiments.util import generate_run_commands, generate_base_command, dict_permutations
 
-PROJECT_NAME = 'TestFrameStacking'
+PROJECT_NAME = 'FirstExperimentDelay'
 
 applicable_configs = {
     'horizon_len': [2 ** 6],
