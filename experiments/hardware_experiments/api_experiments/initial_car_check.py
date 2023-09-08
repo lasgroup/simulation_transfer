8af00d01e87f977ada11781528266af127f2eee7
Jonas Rothfuss <jonas.rothfuss@gmail.com>
2023-09-08 14:32:01 +0200

    improve initial car check and add simulation option

--- experiments/hardware_experiments/api_experiments/initial_car_check.py
+++ experiments/hardware_experiments/api_experiments/initial_car_check.py
@@ -2,8 +2,11 @@ import pickle
 import numpy as np
 import time
 from sim_transfer.hardware.car_env import CarEnv
+from sim_transfer.sims.envs import RCCarSimEnv
 from sim_transfer.sims.util import decode_angles
 from matplotlib import pyplot as plt
+from sim_transfer.sims.util import plot_rc_trajectory, rotate_coordinates
+
 
 """ 
 Script for checking that the rccar is set up properly.
@@ -14,6 +17,9 @@ Instructions:
 """
 
 
+ENCODE_ANGLE = True
+SIMULATION = False
+
 def main():
     # load recorded trajectory
     RECORDING_NAME = f'recording_sep6_carcheck.pickle'
@@ -23,28 +29,46 @@ def main():
     rec_actions = rec_traj.action
 
     # replay action sequence on car
-    env = CarEnv(encode_angle=True, control_frequency=32.7)
-    obs, _ = env.reset()
+    if SIMULATION:
+        env = RCCarSimEnv(encode_angle=ENCODE_ANGLE, use_tire_model=True)
+        obs = env.reset()
+    else:
+        env = CarEnv(encode_angle=ENCODE_ANGLE, max_throttle=0.5, control_time_ms=24.4,
+                     num_frame_stacks=0)
+        obs, _ = env.reset()
     env.step(np.zeros(2))
     t_prev = time.time()
-    observations = [obs]
+    observations = []
+    actions = []
     rewards = []
     time_diffs = []
+    itr = 1
     for i in range(rec_actions.shape[0]):
         action = rec_actions[i]
+
+        # perform step on env
         obs, reward, terminate, info = env.step(action)
+
         t = time.time()
         time_diff = t - t_prev
         t_prev = t
-        print(i, action, reward, time_diff)
-        time_diffs.append(time_diff)
-        rewards.append(reward)
+
+        print('itr:', itr, 'act:', action, 'obs:', obs, 't:', f'{time_diff} sec')
+
+        # append transitions, actions and rewards to trajectory
         observations.append(obs)
-    env.close()
+        actions.append(action)
+        rewards.append(reward)
+        time_diffs.append(time_diff)
+
+        itr += 1
+    if not SIMULATION:
+        env.close()
     observations = np.array(observations)
-    rewards = np.array(rewards)
+    actions = np.array(actions)
     time_diffs = np.array(time_diffs)
 
+    print('Avg time per iter:', np.mean(time_diffs))
     plt.plot(time_diffs)
     plt.title('time diffs')
     plt.show()
@@ -63,7 +87,15 @@ def main():
     plt.title('reward')
     plt.show()
 
-    observations = decode_angles(observations, angle_idx=2)
+    plot_rc_trajectory(observations[..., :(7 if ENCODE_ANGLE else 6)], actions,
+                       encode_angle=ENCODE_ANGLE,
+                       show=True)
+
+    if ENCODE_ANGLE:
+        observations = decode_angles(observations, angle_idx=2)
+
+    rec_observations = rotate_coordinates(rec_observations, encode_angle=True)
+    observations = rotate_coordinates(observations, encode_angle=True)
     # comparison plot recorded and new traj
     fig, axes = plt.subplots(ncols=3, figsize=(12, 4))
     axes[0].plot(rec_observations[:, 0], color='blue', label='rec')
