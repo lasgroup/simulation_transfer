4c3f8a1350a8fc161cb069f114a27ac2a2eec623
Jonas Rothfuss <jonas.rothfuss@gmail.com>
2023-09-08 14:09:44 +0200

    fix bugs and clean up in CarEnv

--- sim_transfer/hardware/car_env.py
+++ sim_transfer/hardware/car_env.py
@@ -6,7 +6,9 @@ import numpy as np
 from gym.spaces import Box
 from typing import Optional
 from sim_transfer.sims.envs import RCCarEnvReward
+from sim_transfer.sims.util import encode_angles, decode_angles
 from typing import Dict, Tuple, Any
+
 
 X_MIN_LIMIT = -1.4
 X_MAX_LIMIT = 2.8
@@ -22,7 +24,7 @@ class CarEnv(gym.Env):
     def __init__(self,
                  ctrl_cost_weight: float = 0.005,
                  margin_factor: float = 10.0,
-                 control_frequency: float = 31.3,
+                 control_time_ms: float = 24.,
                  max_wait_time: float = 1,
                  window_size: int = 6,
                  num_frame_stacks: int = 3,
@@ -37,14 +39,14 @@ class CarEnv(gym.Env):
         self.max_throttle = max_throttle
 
         import carl
-        self.control_frequency = control_frequency
+        self.control_frequency = 1 / (0.001 * control_time_ms)
         self.max_wait_time = max_wait_time
         self.window_size = window_size
         self.num_frame_stacks = num_frame_stacks
         self.port_number = port_number
         self.encode_angle = encode_angle
         self.controller = carl.controller(w_size=window_size, p_number=port_number, wait_time=max_wait_time,
-                                          control_freq=control_frequency)
+                                          control_freq=self.control_frequency)
         self.initial_reset = True
         self.controller_started = False
 
@@ -65,8 +67,12 @@ class CarEnv(gym.Env):
         self.observation_space = Box(low=-high, high=high, shape=(6 + self.encode_angle + 2 * num_frame_stacks,))
         self.action_space = Box(low=-1, high=1, shape=(2,))
 
+        self.action_dim = 2
+        self.state_dim = 7 if self.encode_angle else 6
+
         # init state
-        self.state: np.array = np.zeros((6 + int(self.encode_angle) + 2 * num_frame_stacks,))
+        self.state: np.array = np.zeros(shape=(self.state_dim,))
+        self.stacked_last_actions: np.array = np.zeros(shape=(num_frame_stacks * self.action_dim))
 
     def log_mocap_info(self):
         logs = self.controller.get_mocap_logs()
@@ -99,14 +105,15 @@ class CarEnv(gym.Env):
         current_state[[0, 3]] = current_state[[1, 4]]
         current_state[[1, 4]] = mocap_x
         current_state[2] += np.pi
-        current_state = self.normalize_theta(current_state)
+        current_state = self.normalize_theta_in_state(current_state)
         return current_state
 
     def reset(self):
-        # self.controller.stop()
         if not self.initial_reset:
             self.log_mocap_info()
         self.initial_reset = False
+
+        # dialogue with user
         answer = input("Press Y to continue the reset.")
         assert answer == 'Y' or answer == 'y', "environment execution aborted."
         if not self.controller_started:
@@ -114,29 +121,25 @@ class CarEnv(gym.Env):
             print("Starting controller in ~3 sec")
             time.sleep(3)
             self.controller_started = True
-        current_state = self.get_state_from_mocap()
-        current_state[0:3] = current_state[0:3] - self._goal
+
+        # init last actions to zeros
+        self.stacked_last_actions = np.zeros(2 * self.num_frame_stacks)
+
+        # get current state
+        state = self.get_state_from_mocap()
+        state[0:3] = state[0:3] - self._goal
         if self.encode_angle:
-            new_state = self.get_encoded_state(current_state)
-        else:
-            new_state = current_state
-        initial_actions = np.zeros(2 * self.num_frame_stacks)
-        state = np.concatenate([new_state, initial_actions], axis=0)
+            state = encode_angles(state, angle_idx=self._angle_idx)
         self.state = state
-        return state, {}
+
+        state_with_last_acts = np.concatenate([self.state, self.stacked_last_actions], axis=-1)
+        assert self.observation_space.shape == state_with_last_acts.shape
+        return state_with_last_acts, {}
 
     def close(self):
         self.controller.stop()
         self.log_mocap_info()
         self.controller_started = False
-
-    def get_encoded_state(self, true_state):
-        encoded_state = np.zeros(7)
-        encoded_state[0:2] = true_state[0:2]
-        encoded_state[4:] = true_state[3:]
-        encoded_state[2] = np.sin(true_state[2])
-        encoded_state[3] = np.cos(true_state[2])
-        return encoded_state
 
     def step(self, action: np.array) -> Tuple[np.array, float, bool, Dict[str, Any]]:
         """ Performs one step on the real car (sends commands to)
@@ -144,49 +147,52 @@ class CarEnv(gym.Env):
         Args:
             action: numpy array of shape (2,) with [steer, throttle]
         """
+
+        # check, clip and rescale actions
         assert np.shape(action) == (2,)
-        self.controller.control_mode()  # sets the mode to control
         action = np.clip(action, -1.0, 1.0)
-        action[1] *= self.max_throttle
-        command_set_in_time = self.controller.set_command(action)  # set action
+        scaled_action = action.copy()
+        scaled_action[1] *= self.max_throttle
+
+        # set action command on car and wait for some time
+        self.controller.control_mode()  # sets the mode to control
+        command_set_in_time = self.controller.set_command(scaled_action)  # set action
         assert command_set_in_time, "API blocked python thread for too long"
         time_elapsed = self.controller.get_time_elapsed()  # time elapsed since last action
 
-        # get current state from mocap
-        next_state = self.get_state_from_mocap()  # get state
-        next_state[0:3] = next_state[0:3] - self._goal
-        new_state = np.zeros_like(self.state)
+        # keep last state
+        _last_state = self.state
 
-        # if desired, encode angle
+        # get current state
+        state = self.get_state_from_mocap()
+        state[0:3] = state[0:3] - self._goal
         if self.encode_angle:
-            robot_state = self.get_encoded_state(next_state)
-        else:
-            robot_state = next_state
+            state = encode_angles(state, angle_idx=self._angle_idx)
+        self.state = state
 
         # take care of frame stacking
-        dim_true_state = 6 + int(self.encode_angle)
-        new_state[0:dim_true_state] = robot_state
-        num_previous_actions = 2 * (self.num_frame_stacks - 1)
-        if self.num_frame_stacks > 1:
-            new_state[dim_true_state:num_previous_actions + dim_true_state] = \
-                self.state[2 + dim_true_state:]
-        new_state[num_previous_actions + dim_true_state:] = action  # store the latest action
+        if self.num_frame_stacks > 0:
+            # throw out oldest action and add latest action
+            self.stacked_last_actions = np.concatenate([self.stacked_last_actions[2:], action])
+        assert self.stacked_last_actions.shape == (self.action_dim * self.num_frame_stacks,)
+
+        # add last actions to state
+        state_with_last_acts = np.concatenate([self.state, self.stacked_last_actions], axis=-1)
+        assert self.observation_space.shape == state_with_last_acts.shape
 
         # compute reward
-        reward = self.reward(self.state, action, new_state)
+        reward = self.reward(_last_state, action, state)
 
         # check termination conditions
-        terminate = self.terminate(next_state)
+        terminate = self.terminate(state)
+        return state_with_last_acts, reward, terminate, {'time_elapsed': time_elapsed}
 
-        self.state = new_state
-        return new_state, reward, terminate, {'time_elapsed': time_elapsed}
+    def reward(self, last_state, action, state):
+        return self._reward_model.forward(obs=None, action=action, next_obs=state)
 
-    def reward(self, state, action, next_state):
-        return self._reward_model.forward(obs=None, action=action, next_obs=next_state)
-
-    def terminate(self, next_state):
-        reached_goal = self.reached_goal(next_state, self._goal).item()
-        out_of_bound = self.constraint_violation(next_state)
+    def terminate(self, state):
+        reached_goal = self.reached_goal(state, self._goal)
+        out_of_bound = self.constraint_violation(state)
         time_out = self.env_steps >= self.max_steps
 
         if reached_goal:
@@ -198,8 +204,21 @@ class CarEnv(gym.Env):
         terminate = reached_goal + out_of_bound + time_out
         return terminate
 
+    def reached_goal(self, state: np.array, goal: np.array) -> bool:
+        if self.encode_angle:
+            state = decode_angles(state, angle_idx=self._angle_idx)
+        assert state.shape == (6,)
+        assert goal.shape == (3,)
+
+        dist = np.sqrt(np.square(state[:2] - goal[:2]).sum(-1))
+        ang_dev = np.abs(self.normalize_theta(state[2] - goal[2]))
+        speed = np.sqrt(np.square(state[..., 3:]).sum(-1))
+
+        in_bounds = np.logical_and(np.logical_and(dist < 0.25, ang_dev < 1.0), speed < 1.5)
+        return in_bounds
+
     @staticmethod
-    def constraint_violation(state) -> bool:
+    def constraint_violation(state: np.array) -> bool:
         in_bounds = np.logical_and(
             np.logical_and(X_MIN_LIMIT <= state[0], state[0] <= X_MAX_LIMIT),
             np.logical_and(Y_MIN_LIMIT <= state[1], state[1] <= Y_MAX_LIMIT)
@@ -207,14 +226,11 @@ class CarEnv(gym.Env):
         return not in_bounds
 
     @staticmethod
-    def reached_goal(state, goal) -> bool:
-        dist = np.sqrt(np.square(state[:2] - goal[:2]).sum(-1))
-        ang_dev = np.abs(state[2] - goal[2])
-        speed = np.sqrt(np.square(state[..., 3:]).sum(-1))
-        in_bounds = np.logical_and(np.logical_and(dist < 0.25, ang_dev < 1.0), speed < 1.5)
-        return in_bounds
+    def normalize_theta(theta: np.array) -> np.ndarray:
+        theta = ((theta + np.pi) % (2 * np.pi)) - np.pi
+        return theta
 
-    @staticmethod
-    def normalize_theta(state):
-        state[2] = ((state[2] + np.pi) % (2 * np.pi)) - np.pi
+    def normalize_theta_in_state(self, state: np.array) -> np.array:
+        state[2] = self.normalize_theta(state[2])
         return state
+
