fix numerical issues in sim

sim_transfer/sims/simulators.py
@@ -697,7 +697,7 @@ class RaceCarSim(FunctionSimulator):
         'c_r': 1.27,
         'd_f': 0.02,
         'd_r': 0.017,
-        'i_com': 0.0,
+        'i_com': 0.01,
         'steering_limit': 0.3543
     }
 
@@ -718,7 +718,7 @@ class RaceCarSim(FunctionSimulator):
         'c_r': (1.27, 1.27),
         'd_f': (0.02, 0.02),
         'd_r': (0.017, 0.017),
-        'i_com': (0.0, 0.1),
+        'i_com': (0.01, 0.1),
         'steering_limit': (0.20, 0.5),
     }
 
@@ -1029,55 +1029,44 @@ class StackedActionSimWrapper(FunctionSimulator):
 
 
 if __name__ == '__main__':
-    key = jax.random.PRNGKey(435345)
+    key1, key2 = jax.random.split(jax.random.PRNGKey(435345), 2)
     function_sim = RaceCarSim(use_blend=False, no_angular_velocity=True)
-    function_sim.normalization_stats
-    xs = function_sim.domain.sample_uniformly(key, 100)
-    num_f_samples = 20
-    f_vals = function_sim.sample_function_vals(xs, num_samples=num_f_samples, rng_key=key)
+    x, _ = function_sim._sample_x_data(key1, 1000, 1000)
 
-    NUM_PARALLEL = 20
-    fun_stacked = function_sim.sample_functions(num_samples=NUM_PARALLEL, rng_key=key)
-    # fun_stacked = jax.vmap(function_sim._typical_f)
-    fun_stacked = jax.jit(fun_stacked)
+    f1 = function_sim.sample_function_vals(x, num_samples=10, rng_key=key2)
+    f2 = function_sim._typical_f(x)
+    print(jnp.isnan(f1).any())
+    print(jnp.isnan(f2).any())
 
-    s = jnp.repeat(jnp.array([-9.5005625e-01, -1.4144412e+00, 9.9892426e-01, 4.6371352e-02,
-                              7.2260178e-04, 8.1058703e-03, -7.7542849e-03])[None, :], NUM_PARALLEL, axis=0)
-    traj = [s]
-    actions = []
-    for i in range(60):
-        t = i / 30.
-        a = jnp.array([- 1 * jnp.cos(2 * t), 0.8 / (t + 1)])
-        a = jnp.repeat(a[None, :], NUM_PARALLEL, axis=0)
-        x = jnp.concatenate([s, a], axis=-1)
-        s = fun_stacked(x)
-        traj.append(s)
-        actions.append(a)
-
-    traj = jnp.stack(traj, axis=0)
-    actions = jnp.stack(actions, axis=0)
-    from matplotlib import pyplot as plt
-
-    for i in range(NUM_PARALLEL):
-        plt.plot(traj[:, i, 0], traj[:, i, 1])
-    plt.xlim(-3, 1)
-    plt.ylim(-2, 3)
-    plt.show()
-
-    # key = jax.random.PRNGKey(675)
-    # sim = GaussianProcessSim(input_size=1, output_scale=3.0, mean_fn=lambda x: jnp.squeeze(2 * x, axis=-1))
-    # f_vals = sim.sample_function_vals(x=jax.random.normal(jax.random.PRNGKey(12312), (10, sim.input_size)), num_samples=5, rng_key=key)
-    # sim = RaceCarSim()
-    # sim.sample_function_vals(x=jnp.zeros((1, sim.input_size)), num_samples=5, rng_key=key)
-    # plt, axes = plt.subplots(ncols=1, figsize=(4.5, 4))
-    # for i in range(1):
-    #     x_plot = jnp.linspace(sim.domain.l, sim.domain.u, 200).reshape((-1, 1))
-    #     y_samples = sim.sample_function_vals(x_plot, 10, key)
-    #     for y in y_samples:
-    #         axes.plot(x_plot, y[:, i])
-
-    # axes[0].set_title('Output dimension 1')
-    # axes[1].set_title('Output dimension 2')
-    # x, y = sim.sample_dataset(key, 100, obs_noise_std=0.1, x_support_mode='partial', param_mode='random')
-    # plt.scatter(x, y)
+    # function_sim.normalization_stats
+    # xs = function_sim.domain.sample_uniformly(key, 100)
+    # num_f_samples = 20
+    # f_vals = function_sim.sample_function_vals(xs, num_samples=num_f_samples, rng_key=key)
+    #
+    # NUM_PARALLEL = 20
+    # fun_stacked = function_sim.sample_functions(num_samples=NUM_PARALLEL, rng_key=key)
+    # # fun_stacked = jax.vmap(function_sim._typical_f)
+    # fun_stacked = jax.jit(fun_stacked)
+    #
+    # s = jnp.repeat(jnp.array([-9.5005625e-01, -1.4144412e+00, 9.9892426e-01, 4.6371352e-02,
+    #                           7.2260178e-04, 8.1058703e-03, -7.7542849e-03])[None, :], NUM_PARALLEL, axis=0)
+    # traj = [s]
+    # actions = []
+    # for i in range(60):
+    #     t = i / 30.
+    #     a = jnp.array([- 1 * jnp.cos(2 * t), 0.8 / (t + 1)])
+    #     a = jnp.repeat(a[None, :], NUM_PARALLEL, axis=0)
+    #     x = jnp.concatenate([s, a], axis=-1)
+    #     s = fun_stacked(x)
+    #     traj.append(s)
+    #     actions.append(a)
+    #
+    # traj = jnp.stack(traj, axis=0)
+    # actions = jnp.stack(actions, axis=0)
+    # from matplotlib import pyplot as plt
+    #
+    # for i in range(NUM_PARALLEL):
+    #     plt.plot(traj[:, i, 0], traj[:, i, 1])
+    # plt.xlim(-3, 1)
+    # plt.ylim(-2, 3)
     # plt.show()