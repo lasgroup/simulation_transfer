6802d99cf9dcec584bbf9b331a6c8de0f98b890a
lenarttreven <lenart.treven44@gmail.com>
2023-09-12 14:49:06 +0200

    prepare OfflineTrainingSimVsNoSimComparisonN2

--- experiments/offline_rl_from_recorded_data/launcher.py
+++ experiments/offline_rl_from_recorded_data/launcher.py
@@ -1,7 +1,7 @@
 import exp
 from experiments.util import generate_run_commands, generate_base_command, dict_permutations
 
-PROJECT_NAME = 'OfflineTrainingTestDebugN5'
+PROJECT_NAME = 'OfflineTrainingSimVsNoSimComparisonN2'
 
 applicable_configs = {
     'horizon_len': [100],
