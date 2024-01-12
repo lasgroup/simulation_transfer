7e3ade438c3dc5379656efe8c514f30def65247e
Jonas Rothfuss <jonas.rothfuss@gmail.com>
2024-01-12 16:25:47 +0100

    regression num_data experiments

---
+++ experiments/regression_exp/plots_num_data.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import numpy as np
+import argparse
+
+from typing import Tuple
+from matplotlib import pyplot as plt
+from experiments.util import collect_exp_results, ucb, lcb, median, count
+import math
+
+
+def different_method_plot(df_agg: pd.DataFrame, metric: str = 'nll', display: bool = True,
+                          filter_std_higher_than: float = math.inf) \
+        -> Tuple[pd.DataFrame, plt.Figure]:
+    models = set(df_agg['model'])
+
+    best_rows = []
+    for model in models:
+        df_agg_model = df_agg.loc[df_agg['model'] == model]
+        df_agg_model = df_agg_model[df_agg_model[(metric, 'std')] < filter_std_higher_than]
+        df_agg_model.sort_values(by=(metric, 'mean'), ascending=True, inplace=True)
+        best_rows.append(df_agg_model.iloc[0])
+
+    best_rows_df = pd.DataFrame(best_rows)
+
+    fig, ax = plt.subplots()
+    x_pos = 2 * np.arange(len(models))
+    ax.bar(x_pos, best_rows_df[(metric, 'mean')], color='#ADD8E6')
+    ax.errorbar(x_pos, best_rows_df[(metric, 'mean')], yerr=best_rows_df[(metric, 'std')],
+                linestyle='', capsize=4., color='black')
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(best_rows_df['model'], rotation=-20)
+    ax.set_ylabel(metric)
+    fig.tight_layout()
+
+    if display:
+        plt.show()
+        print(best_rows_df[[('model', ''), ('nll', 'mean'), ('nll', 'std'),
+                            ('rmse', 'mean'), ('rmse', 'std'), ('dirname', '')]].to_string())
+
+    return best_rows_df, fig
+
+
+def main(args, drop_nan=False):
+    df_full, param_names = collect_exp_results(exp_name=args.exp_name)
+    df_full = df_full[df_full['data_source'] == args.data_source]
+    #df_full = df_full[df_full['model'] == args.data_source]
+
+    df_full = df_full[df_full['num_samples_train'] >= 10]
+
+    for col in ['bandwidth_kde', 'bandwidth_ssge', 'bandwidth_score_estim']:
+        if col in df_full.columns:
+            df_full[col] = df_full[col].fillna(value='auto')
+
+    if drop_nan:
+        result_is_nan = df_full['nll'].isna()
+        df_full_dropped = df_full[result_is_nan]
+        print(f'Dropped results due to NaNs: {df_full_dropped.shape[0]}')
+        df_full = df_full[~result_is_nan]
+    else:
+        # replace nan with bad values
+        for col, noise_std in [('nll', 1000.), ('rmse', 5.)]:
+            series = df_full[col]
+            max_value = series.max()
+            is_nan = series.isna()
+            noise = np.random.normal(0, noise_std, size=is_nan.sum())  # Adjust mean and stddev as needed
+            series[is_nan] = max_value + noise
+            df_full[col] = series
+
+
+    # group over everything except seeds and aggregate over the seeds
+    groupby_names = list(set(param_names) - {'model_seed', 'data_seed'})
+
+    # rmove the likelihood_std column since it's a constant list which is not hashable
+    groupby_names.remove('likelihood_std')
+    # groupby_names.remove('added_gp_outputscale')
+    #df_full['added_gp_outputscale'] = df_full['added_gp_outputscale'].apply(lambda x: x[0])
+
+    # replace all the nans in hyperparameter columns with 'N/A'
+    for column in groupby_names:
+        df_full[column] = df_full[column].fillna('N/A')
+
+    # first take mean over the data seeds
+    df_mean = df_full.groupby(by=groupby_names + ['model_seed'], axis=0).mean()
+    df_mean.reset_index(drop=False, inplace=True)
+
+    # then compute the stats over the model seeds
+    df_agg = df_mean.groupby(by=groupby_names, axis=0).aggregate(['mean', 'std', ucb, lcb, median, count], axis=0)
+    df_agg.reset_index(drop=False, inplace=True)
+    # df_agg.sort_values(by=[('nll', 'mean')], ascending=True, inplace=True)
+
+    # filter all the rows where the count is less than 3
+    df_agg = df_agg[df_agg['rmse']['count'] >= 3]
+
+    available_models = set(df_agg['model'])
+    for metric in ['nll', 'rmse']:
+        fig, ax = plt.subplots()
+        for model in available_models:
+            df_model = df_agg[df_agg['model'] == model].sort_values(by=[('num_samples_train', '')], ascending=True)
+            if args.quantile_cis:
+                ax.plot(df_model[('num_samples_train', '')], df_model[(metric, 'median')], label=model)
+                lower_ci = df_model[(metric, 'lcb')]
+                upper_ci = df_model[(metric, 'ucb')]
+            else:
+                ax.plot(df_model[('num_samples_train', '')], df_model[(metric, 'mean')], label=model)
+                CI_width = 2 * np.sqrt(df_model[(metric, 'count')])
+                lower_ci = df_model[(metric, 'mean')] - CI_width * df_model[(metric, 'std')]
+                upper_ci = df_model[(metric, 'mean')] + CI_width * df_model[(metric, 'std')]
+            ax.fill_between(df_model[('num_samples_train', '')], lower_ci, upper_ci, alpha=0.3)
+        ax.set_title(f'{args.data_source} - {metric}')
+        #ax.set_xscale('log')
+        #ax.set_ylim((-3.8, -2.))
+
+        ax.legend()
+        fig.show()
+    print('Models:', set(df_agg['model']))
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Inspect results of a regression experiment.')
+    parser.add_argument('--exp_name', type=str, default='jan10_num_data')
+    parser.add_argument('--quantile_cis', type=int, default=1)
+    parser.add_argument('--data_source', type=str, default='pendulum')
+    args = parser.parse_args()
+    main(args)
--- experiments/regression_exp/sweep_regression_exp_num_data.py
+++ experiments/regression_exp/sweep_regression_exp_num_data.py
@@ -10,17 +10,17 @@ import os
 
 MODEL_SPECIFIC_CONFIG = {
     'BNN_SVGD': {
-        'bandwidth_svgd': {'distribution': 'log_uniform', 'min': -1., 'max': 4.},
-        'num_train_steps': {'values': [5000, 10000, 20000]},
+        'bandwidth_svgd': {'values': [5.]},
+        'num_train_steps': {'values': [20000]},
     },
     'BNN_FSVGD': {
-        'bandwidth_svgd': {'distribution': 'log_uniform_10', 'min': -1.0, 'max': 0.0},
-        'bandwidth_gp_prior': {'distribution': 'log_uniform', 'min': -2., 'max': 0.},
-        'num_train_steps': {'values': [5000, 10000, 20000]},
+        'bandwidth_svgd': {'values': [0.2]},
+        'bandwidth_gp_prior': {'values': [0.4]},
+        'num_train_steps': {'values': [20000]},
         'num_measurement_points': {'values': [32]},
     },
     'BNN_FSVGD_SimPrior_gp': {
-        'bandwidth_svgd': {'distribution': 'log_uniform_10', 'min': -1.0, 'max': 0.0},
+        'bandwidth_svgd': {'values': [0.2]},
         'num_train_steps': {'values': [40000]},
         'num_measurement_points': {'values': [32]},
         'num_f_samples': {'values': [1024]},
@@ -33,26 +33,17 @@ MODEL_SPECIFIC_CONFIG = {
         'bandwidth_score_estim': {'distribution': 'log_uniform_10', 'min': -0.5, 'max': 1.},
     },
     'BNN_FSVGD_SimPrior_nu-method': {
-        'bandwidth_svgd': {'distribution': 'log_uniform_10', 'min': -1.0, 'max': 0.0},
-        'num_train_steps': {'values': [30000, 40000, 50000]},
+        'bandwidth_svgd': {'values': [0.2]},
+        'num_train_steps': {'values': [60000]},
         'num_measurement_points': {'values': [32]},
         'num_f_samples': {'values': [512]},
-        #'bandwidth_score_estim': {'distribution': 'log_uniform_10', 'min': -0.5, 'max': 0.5},
-        'bandwidth_score_estim': {'distribution': 'uniform', 'min': 1.0, 'max': 2.0},
+        'bandwidth_score_estim': {'values': [1.2]},
     },
     'BNN_FSVGD_SimPrior_kde': {
-        'bandwidth_svgd': {'distribution': 'log_uniform_10', 'min': -1.0, 'max': 1.0},
-        'num_train_steps': {'values': [60000, 80000, 100000]},
-        'num_measurement_points': {'values': [16, 32]},
-        'num_f_samples': {'values': [1024, 2056]},
-    },
-    'BNN_FSVGD_SimPrior_gp+nu-method': {
-        'bandwidth_svgd': {'distribution': 'log_uniform_10', 'min': -1.0, 'max': 0.0},
+        'bandwidth_svgd': {'values': [0.2]},
         'num_train_steps': {'values': [80000]},
-        'num_measurement_points': {'values': [16, 32]},
-        'num_f_samples': {'values': [512]},
-        'switch_score_estimator_frac': {'values': [0.6667]},
-        'bandwidth_score_estim': {'distribution': 'log_uniform_10', 'min': 0.0, 'max': 0.5},
+        'num_measurement_points': {'values': [32]},
+        'num_f_samples': {'values': [2056]},
     },
     'BNN_SVGD_DistillPrior': {
         'bandwidth_svgd': {'distribution': 'log_uniform', 'min': -2., 'max': 2.},
@@ -72,7 +63,6 @@ def main(args):
 
     sweep_config = {
         'data_source': {'value': args.data_source},
-        'num_samples_train': DATASET_CONFIGS[args.data_source]['num_samples_train'],
         'model': {'value': args.model},
         'learn_likelihood_std': {'value': args.learn_likelihood_std},
         'pred_diff': {'value': args.pred_diff},
@@ -87,20 +77,31 @@ def main(args):
     exp_base_path = os.path.join(RESULT_DIR, args.exp_name)
     exp_path = os.path.join(exp_base_path, f'{args.data_source}_{args.model}')
 
+    if args.data_source == 'racecar':
+        N_SAMPLES_LIST = [50, 100, 200, 400, 800, 1600, 3200]
+    elif args.data_source == 'pendulum':
+        N_SAMPLES_LIST = [10, 20, 40, 80, 160, 320, 640]
+    else:
+        raise NotImplementedError(f'Unknown data source {args.data_source}.')
+
     command_list = []
     output_file_list = []
     for _ in range(args.num_hparam_samples):
         flags = sample_param_flags(sweep_config)
+        exp_hash = hash_dict(flags)
 
-        exp_result_folder = os.path.join(exp_path, hash_dict(flags))
-        flags['exp_result_folder'] = exp_result_folder
+        for num_samples_train in N_SAMPLES_LIST:
 
-        for model_seed, data_seed in itertools.product(model_seeds[:args.num_model_seeds],
-                                                       data_seeds[:args.num_data_seeds]):
-            cmd = generate_base_command(experiments.regression_exp.run_regression_exp,
-                                        flags=dict(**flags, **{'model_seed': model_seed, 'data_seed': data_seed}))
-            command_list.append(cmd)
-            output_file_list.append(os.path.join(exp_result_folder, f'{model_seed}_{data_seed}.out'))
+            exp_result_folder = os.path.join(exp_path, f'{exp_hash}_{num_samples_train}')
+            flags['exp_result_folder'] = exp_result_folder
+
+            for model_seed, data_seed in itertools.product(model_seeds[:args.num_model_seeds],
+                                                           data_seeds[:args.num_data_seeds]):
+                cmd = generate_base_command(experiments.regression_exp.run_regression_exp,
+                                            flags=dict(**flags, **{'model_seed': model_seed, 'data_seed': data_seed,
+                                                                   'num_samples_train': num_samples_train}))
+                command_list.append(cmd)
+                output_file_list.append(os.path.join(exp_result_folder, f'{model_seed}_{data_seed}.out'))
 
     generate_run_commands(command_list, output_file_list, num_cpus=args.num_cpus,
                           num_gpus=1 if args.gpu else 0, mode=args.run_mode, prompt=not args.yes)
@@ -111,9 +112,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Meta-BO run')
 
     # sweep args
-    parser.add_argument('--num_hparam_samples', type=int, default=20)
-    parser.add_argument('--num_model_seeds', type=int, default=3, help='number of model seeds per hparam')
-    parser.add_argument('--num_data_seeds', type=int, default=3, help='number of model seeds per hparam')
+    parser.add_argument('--num_hparam_samples', type=int, default=1)
+    parser.add_argument('--num_model_seeds', type=int, default=5, help='number of model seeds per hparam')
+    parser.add_argument('--num_data_seeds', type=int, default=5, help='number of model seeds per hparam')
     parser.add_argument('--num_cpus', type=int, default=1, help='number of cpus to use')
     parser.add_argument('--run_mode', type=str, default='euler')
 
@@ -127,7 +128,7 @@ if __name__ == '__main__':
     parser.add_argument('--data_source', type=str, default='racecar')
 
     # # standard BNN parameters
-    parser.add_argument('--model', type=str, default='BNN_FSVGD_SimPrior_kde')
+    parser.add_argument('--model', type=str, default='BNN_SVGD')
     parser.add_argument('--learn_likelihood_std', type=int, default=0)
     parser.add_argument('--pred_diff', type=int, default=1)
 
